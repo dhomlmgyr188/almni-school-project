99004925281e861bb6af6342bcc726c2f7391d2f
dhme <dhme@gmail.com>
2026-03-10 20:48:32 +0300

    تحزيم وتهيئة الملفات html + css + files + sass + images + bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-console.log("print");
+import * as bootstrap from 'bootstrap'
+import "../node_modules/bootstrap/scss/bootstrap.scss";
+import "./sass/style.scss"
+import "./css/style.css";
 
-
-console.log("log");
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item=> new bootstrap.Tooltip(item))
+    
+console.log("hello-world")
+console.log("hello-world")
+console.log("hello-world")
+console.log("hello-world")
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import autoprefixer from 'autoprefixer';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin"; 
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 // For compatibility with older Node.js versions
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const pages = ["index", "login", "register", "student_first", "student_second"];
+
+
 
 export default {
   mode: "development",
@@ -13,37 +19,82 @@ export default {
     path: path.resolve(__dirname, "./dist"),
     filename: "main.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "login.html",
-      template: "./src/login.html",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "register.html",
-      template: "./src/register.html",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "student_first.html",
-      template: "./src/student_first.html",
-    }),
-    new HtmlWebpackPlugin({
-      filename: "student_second.html",
-      template: "./src/student_second.html",
-    }),
-  ],
- module: {
+  module: {
     rules: [
       {
         test: /\.html$/i,
         loader: "html-loader",
         options: {
+          esModule: false,
           minimize: true,
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [ 
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "autoprefixer",
+                  "rtlcss"
+                ],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./images/[name][ext]"
+        }
+      },
+      {
+        test: /\.(eot|woff2|woff|ttf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./fonts/[name][ext]"
+        }
+      },
     ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack v5, you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/style.css",
+    }),
+  new MiniCssExtractPlugin({ filename: "./css/style.css" }),
+    ...pages.map(page => new HtmlWebpackPlugin({
+      filename: `${page}.html`,
+      template: `./src/${page}.html`,
+    }))
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    hot: false,
+    open: true,
+    compress: true,
+    port: 9000,
+    devMiddleware: {
+    writeToDisk: true, 
+  }
   },
 };
